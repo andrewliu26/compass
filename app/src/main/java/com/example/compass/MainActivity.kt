290a3d81c4ce2a5fa5accf1f1cbd18193146e6bc
shiro-base <txzhou00@gmail.com>
2023-10-16 21:46:58 -0400

    shaking functionality added

--- app/src/main/java/com/example/compass/MainActivity.kt
+++ app/src/main/java/com/example/compass/MainActivity.kt
@@ -1,21 +1,50 @@
 package com.example.compass
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import kotlin.math.sqrt
 import android.os.Bundle
 import android.view.GestureDetector
 import android.view.MotionEvent
+import android.animation.ObjectAnimator
+import android.view.animation.CycleInterpolator
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GestureDetectorCompat
+import java.util.Objects
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView1:TextView
+    private lateinit var textView2:TextView
     private lateinit var gestureDetector: GestureDetectorCompat
+    private lateinit var sensorManager: SensorManager
+    private var acceleration = 0f
+    private var currentAcceleration = 0f
+    private var lastAcceleration = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         gestureDetector = GestureDetectorCompat(this, FlingGestureListener(this))
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        Objects.requireNonNull(sensorManager)!!
+            .registerListener(sensorListener, sensorManager!!
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
+
+        currentAcceleration = SensorManager.GRAVITY_EARTH
+        lastAcceleration = SensorManager.GRAVITY_EARTH
+
+        textView1 = findViewById(R.id.Home)
+        textView2 = findViewById(R.id.prompt)
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
@@ -49,4 +78,50 @@ class MainActivity : AppCompatActivity() {
             return true
         }
     }
+
+
+    private val sensorListener: SensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+            lastAcceleration = currentAcceleration
+
+            currentAcceleration = sqrt((x * x + y * y + z * z).toDouble()).toFloat()
+            val delta: Float = currentAcceleration - lastAcceleration
+            acceleration = acceleration * 0.9f + delta
+
+            if (acceleration > 2.0f) {
+                shakeTextView(textView1)
+                shakeTextView(textView2)
+            }
+        }
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+    }
+    override fun onResume() {
+        sensorManager?.registerListener(sensorListener, sensorManager!!.getDefaultSensor(
+            Sensor .TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL
+        )
+        super.onResume()
+    }
+
+    override fun onPause() {
+        sensorManager!!.unregisterListener(sensorListener)
+        super.onPause()
+    }
+
+    private fun shakeTextView(textView: TextView) {
+        val shakeAnimator = ObjectAnimator.ofFloat(textView, "translationX", 0f, 10f, 0f, -10f)
+        shakeAnimator.duration = 2000 // 2 seconds
+        shakeAnimator.interpolator = CycleInterpolator(80f)
+        shakeAnimator.start()
+    }
+
 }
+
+
+
+
+
+
--- app/src/main/java/com/example/compass/NorthActivity.kt
+++ app/src/main/java/com/example/compass/NorthActivity.kt
@@ -1,21 +1,47 @@
 package com.example.compass
 
+import android.animation.ObjectAnimator
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.GestureDetector
 import android.view.MotionEvent
+import android.view.animation.CycleInterpolator
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GestureDetectorCompat
+import java.util.Objects
+import kotlin.math.sqrt
 
 class NorthActivity : AppCompatActivity() {
 
+    private lateinit var textView1:TextView
     private lateinit var gestureDetector: GestureDetectorCompat
+    private lateinit var sensorManager: SensorManager
+    private var acceleration = 0f
+    private var currentAcceleration = 0f
+    private var lastAcceleration = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_north)
         gestureDetector = GestureDetectorCompat(this, SwipeGestureListener(this))
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        Objects.requireNonNull(sensorManager)!!
+            .registerListener(sensorListener, sensorManager!!
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
+
+        currentAcceleration = SensorManager.GRAVITY_EARTH
+        lastAcceleration = SensorManager.GRAVITY_EARTH
+
+        textView1 = findViewById(R.id.north)
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
@@ -34,5 +60,42 @@ class NorthActivity : AppCompatActivity() {
             }
             return true
         }
+    }
+
+    private val sensorListener: SensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+            lastAcceleration = currentAcceleration
+
+            currentAcceleration = sqrt((x * x + y * y + z * z).toDouble()).toFloat()
+            val delta: Float = currentAcceleration - lastAcceleration
+            acceleration = acceleration * 0.9f + delta
+
+            if (acceleration > 2.0f) {
+                shakeTextView(textView1)
+            }
+        }
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+    }
+    override fun onResume() {
+        sensorManager?.registerListener(sensorListener, sensorManager!!.getDefaultSensor(
+            Sensor .TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL
+        )
+        super.onResume()
+    }
+
+    override fun onPause() {
+        sensorManager!!.unregisterListener(sensorListener)
+        super.onPause()
+    }
+
+    private fun shakeTextView(textView: TextView) {
+        val shakeAnimator = ObjectAnimator.ofFloat(textView, "translationX", 0f, 10f, 0f, -10f)
+        shakeAnimator.duration = 2000 // 2 seconds
+        shakeAnimator.interpolator = CycleInterpolator(80f)
+        shakeAnimator.start()
     }
 }
